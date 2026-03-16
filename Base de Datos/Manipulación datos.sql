24a82b12ff328ac2be4b250fde691bafed0dd27e
Miguel-Gil-Moral <mgilm2324@politecnics.barcelona>
2026-03-16 11:30:57 +0100

    Usuarios creados por parte de Miguel

--- Base de Datos/Manipulación datos.sql
+++ Base de Datos/Manipulación datos.sql
@@ -1,18 +1,41 @@
 use football_manager;
 
-/*Donat el nom d'una lliga i la temporada. Fer que tots els partits de la jornada 5 acabin en 0-0.*/
+/*(MARIOOOOOOOO_____!!!!!!!!!!!!!!!!!!____________________!!!!!!!!!!!!!!!!!!!_____________________!!!!!!!!!!!!!!!!!!!!!!)1- Donat el nom d'una lliga i la temporada. 
+Fer que tots els partits de la jornada 5 acabin en 0-0.*/
 
-/*La nostra base de dades està creixent molt i hem d'alliberar una mica d'espai. Esborra totes les ciutats en les quals no hi hagi cap equip.*/
+/*(MARIOOOOOOOO_____!!!!!!!!!!!!!!!!!!____________________!!!!!!!!!!!!!!!!!!!_____________________!!!!!!!!!!!!!!!!!!!!!!)2- La nostra base de dades està creixent molt i hem d'alliberar una mica d'espai. 
+Esborra totes les ciutats en les quals no hi hagi cap equip.*/
 
-/*Baixa un 10% la motivació d'aquells jugadors que cobren menys de 5 milions i tenen 35 anys o més.*/
+/*(MARIOOOOOOOO_____!!!!!!!!!!!!!!!!!!____________________!!!!!!!!!!!!!!!!!!!_____________________!!!!!!!!!!!!!!!!!!!!!!)3- Baixa un 10% la motivació d'aquells jugadors que cobren menys de 5 milions i tenen 35 anys o més.*/
 
-/*S'ha celebrat un entrenament especial de porters dels primers equips. Puja en 3 punts la qualitat de tots els porters de la base de dades que no pertanyin a un equip filial.*/
+/*(MARIOOOOOOOO_____!!!!!!!!!!!!!!!!!!____________________!!!!!!!!!!!!!!!!!!!_____________________!!!!!!!!!!!!!!!!!!!!!!)4- S'ha celebrat un entrenament especial de porters dels primers equips. 
+Puja en 3 punts la qualitat de tots els porters de la base de dades que no pertanyin a un equip filial.*/
 
-/*Afegeix un camp a la taula "Jugadors" per indicar si el jugador està disponible per a ser fitxat. De moment posarem com a disponibles tots aquells jugadors que hagin estat contractats durant el primer trimestre del 2024. La resta de jugadors de la base de dades hauran d'aparèixer com a no disponibles per a ser fitxats.*/
+/*✅ 5- Afegeix un camp a la taula "Jugadors" per indicar si el jugador està disponible per a ser fitxat. 
+De moment posarem com a disponibles tots aquells jugadors que hagin estat contractats durant el primer trimestre del 2024. 
+La resta de jugadors de la base de dades hauran d'aparèixer com a no disponibles per a ser fitxats.*/
 
-/*Estem a punt de crear una nova lliga. Hi participaran 6 equips al llarg de 6 jornades. Realitza els INSERT necessaris.*/
+alter table jugadors
+add disponible tinyint not null;
 
-/*Crea els INSERT necessaris per poder afegir una nova jornada de lliga amb un partit en què hi hagi algun gol.*/
+update jugadors 
+inner join jugadors_equips on jugadors.persones_id = jugadors_equips.jugadors_id
+set disponible = 1
+where jugadors_equips.data_fitxatge between '2024-01-01' and '2024-03-31';
 
-/*Crea els INSERT necessaris per poder donar un nou equip d'altra a la nostra base de dades, incloent el seu estadi, l'entrenador i 2 jugadors.*/
+select * from persones;
+
+select * from jugadors;
+
+/*6- Estem a punt de crear una nova lliga. 
+Hi participaran 6 equips al llarg de 6 jornades.
+Realitza els INSERT necessaris.*/
+
+
+
+/*7- Crea els INSERT necessaris per poder afegir una nova jornada de lliga amb un partit en què hi hagi algun gol.*/
+
+
+
+/*8- Crea els INSERT necessaris per poder donar un nou equip d'altra a la nostra base de dades, incloent el seu estadi, l'entrenador i 2 jugadors.*/
 
--- Base de Datos/Usuarios y privilegios.sql
+++ Base de Datos/Usuarios y privilegios.sql
@@ -1,13 +1,11 @@
-use football_manager;
-
-/*1- Usuari superadministrador. 
+/*✅ 1- Usuari superadministrador. 
 Aquest usuari ha de poder realitzar operacions de lectura, inserció, modificació i esborrat sobre tota la base de dades. 
 Cal que la seva contrasenya es renovi cada mes i no pot fer servir les dues darreres contrasenyes.*/
 create user 'superadministrador'@'localhost'
 password expire interval 30 day
 password history 2;
 
-grant all privileges on *.* to 'superadministrador'@'localhost';
+grant all privileges on football_manager.* to 'superadministrador'@'localhost';
 
 /*2- Usuari adminEquips. 
 Aquest usuari he de poder consultar i modificar les dades dels equips, els seus estadis i les persones que en formen part. 
@@ -19,14 +17,22 @@ Aquest usuari s'encarregarà de la gestió completa (afegir, modificar i consult
 Per criteris de seguretat, només podrà realitzar dues modificacions a la base de dades cada hora. 
 D'aquesta manera, si les credencials quedessin compromoses, evitem ensurts majors.*/
 
-/*Rol periodista. 
+/*✅ 4- Rol periodista. 
 L'accés a la informació de la base de dades es monetitza en part donant accés als periodistes esportius. 
 Aquests tenen accés de consulta d'informació a tota la base de dades.*/
 
 create role if not exists 'periodista';
 
-/*4- Crea l'usuari periodistaSport, periodistaAS i periodistaMundo.*/
+grant 'periodista' to 'periodistaSport'@'localhost', 'periodistaAS'@'localhost', 'periodistaMundo'@'localhost';
 
-/*5- Els alts càrrecs del diari AS han aconseguit que els seus periodistes disposin d'accés preferent a les dades, que se'ls serviran processades per a un millor anàlisi. 
+grant select on football_manager.* to 'periodista';
+
+/*✅ 5- Crea l'usuari periodistaSport, periodistaAS i periodistaMundo.*/
+
+create user 'periodistaSport'@'localhost';
+create user 'periodistaAS'@'localhost';
+create user 'periodistaMundo'@'localhost';
+
+/*6- Els alts càrrecs del diari AS han aconseguit que els seus periodistes disposin d'accés preferent a les dades, que se'ls serviran processades per a un millor anàlisi. 
 Necessiteu crear almenys 4 vistes que mostrin informació ja filtrada i processada (podeu inspirar-vos en algunes de les consultes que us demanem al repte). 
 Per exemple: un informe dels millors llançadors de penaltis. Només l'usuari periodistaAS tindrà accés a aquests informes.*/
