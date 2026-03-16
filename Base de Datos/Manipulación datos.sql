1e6d5e7ea0ba83d3c710026eb7b504c272f169cf
Miguel-Gil-Moral <mgilm2324@politecnics.barcelona>
2026-03-16 23:51:41 +0100

    Update Manipulación datos.sql

--- Base de Datos/Manipulación datos.sql
+++ Base de Datos/Manipulación datos.sql
@@ -35,19 +35,20 @@ Realitza els INSERT necessaris.*/
 
 /*7- Crea els INSERT necessaris per poder afegir una nova jornada de lliga amb un partit en què hi hagi algun gol.*/
 
-insert into jornada;
+-- insert into jornada;-- 
 
 select * from jornades;
 
-/*8- Crea els INSERT necessaris per poder donar un nou equip d'altra a la nostra base de dades, incloent el seu estadi, l'entrenador i 2 jugadors.*/
+/*✅ 8- Crea els INSERT necessaris per poder donar un nou equip d'altra a la nostra base de dades, incloent el seu estadi, l'entrenador i 2 jugadors.*/
 
 insert into persones values (199, 'Miguel', 'Gil', '2005-02-17', 90, 10000000, 'jugador');
 insert into persones values (200, 'Darrell', 'Zapata', '2004-01-05', 89, 1, 'jugador');
 insert into persones values (201, 'David', 'Garcia', '2003-12-16', 88, 0, 'entrenador');
 insert into entrenadors values (201, 0, 0);
-insert into jugadors values (199, 13, 90, 3, 1);
-insert into jugadors values (200, 12, 89, 2, 1);
-insert into equips values (38, 'Equipo nuevo', 2026, 'Pepito los palotes', 1, 1, null);
+insert into jugadors values (199, 13, 90, 3);
+insert into jugadors values (200, 12, 89, 2);
+insert into estadis values (34, 'En algun lugar de aqui', 1);
+insert into equips values (38, 'Equipo nuevo', 2026, 'Pepito los palotes', 1, 34, null);
 insert into jugadors_equips values ('2024-02-17', 199, 38, null);
 insert into jugadors_equips values ('2024-01-05', 200, 38, null);
 insert into entrenar_equips values ('2024-03-16', 201, 38, null);
